9840dde6ec376c058ea3ae254e02fa5a95bd7a0d
amalinowski <antonimalinowski.kontakt@gmail.com>
2023-02-24 21:48:37 +0100

    Playing withaudio visualization, audio rendering issue?

--- src/Composition.tsx
+++ src/Composition.tsx
@@ -1,21 +1,51 @@
-import { useCurrentFrame, AbsoluteFill, useVideoConfig } from "remotion";
+import { useAudioData, visualizeAudio } from "@remotion/media-utils";
+import { useCurrentFrame, AbsoluteFill, useVideoConfig, Audio, staticFile } from "remotion";
+import Himalaya from "../public/Himalaya.mp3";
 
 export const MyComposition = () => {
   const frame = useCurrentFrame();
 	const { fps, durationInFrames, width, height } = useVideoConfig();
+	const audioData = useAudioData(Himalaya);
 
+	if (!audioData) {
+    return null;
+  }
+
+	const visualization = visualizeAudio({
+    fps,
+    frame,
+    audioData,
+    numberOfSamples: 32,
+  }); // [0.22, 0.1, 0.01, 0.01, 0.01, 0.02, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+
+	// Render a bar chart for each frequency, the higher the amplitude,
+  // the longer the bar
   return (
-    <AbsoluteFill
-      style={{
-        justifyContent: "center",
-        alignItems: "center",
-        fontSize: 100,
-				color: 'orange',
-        backgroundColor: "green"
-      }}
-    >
-      The current frame is {frame}.
-			The {width}x{height}px video is {durationInFrames / fps} seconds long.
-    </AbsoluteFill>
+		<div>
+			
+			<AbsoluteFill
+				style={{
+					justifyContent: "center",
+					alignItems: "center",
+					fontSize: 100,
+					color: 'blue',
+					backgroundColor: "green"
+				}}
+			>
+				<Audio src={Himalaya} startFrom={1100} endAt={3200}/>
+      
+				{visualization.map((v) => {
+        return (
+          <div
+            style={{ width: 10000 * v, height: 15, backgroundColor: "purple" }}
+          />
+        );
+      })}
+			
+				The current frame is {frame}.
+				The {width}x{height}px video is {durationInFrames / fps} seconds long.
+				{/* <Audio src={staticFile("Himalaya.mp3")} /> */}
+			</AbsoluteFill>
+		</div>
   );
 };
--- src/Root.tsx
+++ src/Root.tsx
@@ -7,7 +7,7 @@ export const RemotionRoot: React.FC = () => {
 			<Composition
 				id="MyComp"
 				component={MyComposition}
-				durationInFrames={60}
+				durationInFrames={1200}
 				fps={30}
 				width={1920}
 				height={1080}
