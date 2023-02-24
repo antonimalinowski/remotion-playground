d337e118af3a515877a702f685168aaf2d4b59b7
amalinowski <antonimalinowski.kontakt@gmail.com>
2023-02-24 20:39:28 +0100

    First composition

--- src/Composition.tsx
+++ src/Composition.tsx
@@ -1,3 +1,20 @@
+import { useCurrentFrame, AbsoluteFill, useVideoConfig } from "remotion";
+
 export const MyComposition = () => {
-	return null;
+  const frame = useCurrentFrame();
+	const { fps, durationInFrames, width, height } = useVideoConfig();
+
+  return (
+    <AbsoluteFill
+      style={{
+        justifyContent: "center",
+        alignItems: "center",
+        fontSize: 100,
+        backgroundColor: "white"
+      }}
+    >
+      The current frame is {frame}.
+			The {width}x{height}px video is {durationInFrames / fps} seconds long.
+    </AbsoluteFill>
+  );
 };
